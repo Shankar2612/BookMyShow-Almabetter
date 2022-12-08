463e6d90d223b0c99245acae292f2b15d18c9e9a
unknown <bapu.2612@gmail.com>
2022-12-08 21:36:17 +0530

    added .env file

--- back-end/connector.js
+++ back-end/connector.js
@@ -1,22 +1,23 @@
-
+require("dotenv").config();
 const { MongoClient } = require("mongodb");
 
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
 // for production.
-const mongoURI = "mongodb+srv://VIKAS:VIKAS@firstcluster.1fyyzpk.mongodb.net/bookMovie?retryWrites=true&w=majority";
+const mongoURI = process.env.MONGODBURI;
 
 // for development.
 // const mongoURI = "mongodb://localhost:27017/" + "bookMovie"
-const connectToMongo = async() => {
-
-    // Connecting to database using connection string and speciying if there is any error or it was successfull
-    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => { console.log("connection established with mongodb server online"); })
-    .catch(err => {
-        console.log("error while connection", err)
+const connectToMongo = async () => {
+  // Connecting to database using connection string and speciying if there is any error or it was successfull
+  mongoose
+    .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("connection established with mongodb server online");
+    })
+    .catch((err) => {
+      console.log("error while connection", err);
     });
-}
-
+};
 
 exports.connection = connectToMongo;
